7f0ee7c14e343ef5f1af6c7903a48bd60faec726
Jay Keraliya <jaykeraliya0@gmail.com>
2023-05-16 09:52:59 +0530

    fixed lag at nfts/[slug] page

--- app/nfts/[slug]/layout.tsx
+++ app/nfts/[slug]/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function NFTsLayout({
   children,
@@ -6,7 +7,14 @@ export default function NFTsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-[url('/images/background.jpg')] bg-cover bg-fixed bg-black/80 bg-blend-multiply">
+    <div className="relative min-h-screen bg-cover bg-fixed bg-black/80 bg-blend-multiply">
+      <Image
+        width={1920}
+        height={1080}
+        className="fixed top-0 object-center object-cover pointer-events-none -z-40"
+        src="/images/background.jpg"
+        alt="Background"
+      />
       <Header />
       {children}
     </div>

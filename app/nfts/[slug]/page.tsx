4f09bcb5ee801cd9d1018cc31b19c0ab2ee64fc6
Jay Keraliya <jaykeraliya0@gmail.com>
2023-05-19 18:13:21 +0530

    Fix /nfts/[slug] design at mobile level Fixes #21

--- app/nfts/[slug]/page.tsx
+++ app/nfts/[slug]/page.tsx
@@ -52,7 +52,7 @@ const NFTs = async ({ params }: Props) => {
   const data: NFT[] = await getData(address);
   
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-24 mt-20 py-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-5 sm:mx-10 mt-20 py-10">
       {data.length > 0 ? (
         data.map((item) => <NFTCard key={item.name} data={item} />)
       ) : (

f74945b601fe957da42630bb40ea93bae4e30531
Jay Keraliya <jaykeraliya0@gmail.com>
2023-05-27 09:47:05 +0530

    Write test for smart contract Fixes #27

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,11 +1,12 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.1",
-  defaultNetwork: "goerli",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
     goerli: {
